bf5a3e351086e0df04bb556daf80d25f9e9b0c17
mbazzw <cnjszzw@qq.com>
2026-05-24 22:30:58 +0800

    fix: 添加 @EnableElasticsearchRepositories 扫描 ES Repository 包

--- bilibili-content-service/src/main/java/com/bilibili/content/ContentServiceApplication.java
+++ bilibili-content-service/src/main/java/com/bilibili/content/ContentServiceApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableTransactionManagement
 @MapperScan("com.imooc.bilibili.dao")
+@EnableElasticsearchRepositories(basePackages = "com.imooc.bilibili.dao.repository")
 @ComponentScan(basePackages = {"com.bilibili.content", "com.imooc.bilibili"})
 public class ContentServiceApplication {
     public static void main(String[] args) {
